96f989597177481f89fcfb0c7c7317e1401d23cd
micahmura21 <Mickmura21@gmail.com>
2017-04-22 11:56:44 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.body.addEventListener('keydown', createLetters);
 var alphabet =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
 var emptystring="";
 var supriseWords =["lambs","jerry"];
@@ -14,4 +15,26 @@ function startGame(){
 	document.getElementById('word-container').innerHTML += "_";
 	}
 }
+function createLetters(e){
+	console.log(e.key);
+	for (var i = 0; i<alphabet.lenght;  i++) { 
+		if(alphabet=== e.key){
+			checkForWord(alphabet[i]);
+		console.log('they match')
+		}
+	}
+}
+
+
+
 startGame();
+createLetters();
+function checkForWord(letters){
+	for (var i = 0; i < wordToGuess.length; i++){
+		if (wordToGuess[1] === letter) {
+			chnageInnerHTML[i] = letter;
+			doccument.getElementById('word-container').innerHTML = chnageInnerHTML;
+			console.log ("This letter is in the word");
+		}
+	}
+}
